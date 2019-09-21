fe911d1a3d25621706f9c2062d84d9e68fb78605
pesl0523 <pesl0523@pesl0523ui-iMac.local>
2019-09-21 12:38:21 +0900

    add logs

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const ecc = require('eosjs-ecc');
 
 // Get public key from eos format string
-var pubkey = ecc.PublicKey.fromStringOrThrow('EOS88RHBHuWDn49WiD49wBzVcVndu2yuEkqjLyapcFxgfTWvz1baX');
+var pubkey = ecc.PublicKey.fromStringOrThrow('EOS7pU1r3AVJwuyyLDcFhZCMJa1V1JWn5LgWp6zJYkGzLScNLsaNR');
+console.log('PublicKey:', pubkey.toString());
 
 // Verify computed signature
 var message = 'test_message';
-var signature = ecc.Signature.fromStringOrThrow('SIG_K1_KcGXnHzt86YyHtfsHJiZv7tbepGyvZfLLQLFj3ZxDNQzZmXuJqnggMWKhKvF9PMdDavBs4EmbKTFK4LpNbUamYJ4iQywWz');
-console.log('Signature  String : ', signature.toString());
-console.log('Signature  Verify : ', ecc.verify(signature, message, pubkey));
+console.log('Message  :', message);
+
+var signature = ecc.Signature.fromStringOrThrow('SIG_K1_L2SBxTLa3pi2ckcjGbZ7Yo7r9SrDX46SWJKTLtNV4SkBC8CiamjKZmif18Ys936EQ2sQQPtxagKyDb4wUv7wxscsKcgy9h');
+console.log('Signature:', signature.toString());
+console.log('verify is', ecc.verify(signature, message, pubkey));
